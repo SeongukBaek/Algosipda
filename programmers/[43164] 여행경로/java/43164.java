e16a04cde61a66192a09e020723595281c308aaa
SUbbb <33208303+SeongukBaek@users.noreply.github.com>
2025-03-17 22:43:53 +0900

    [programmers] 여행경로 (Time: 117.83ms, Memory: 114MB) - by Code-Vault

---
+++ programmers/[43164] 여행경로/java/43164.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    private int ticketCount;
+    private boolean[] isVisited;
+    private final List<String> routes = new ArrayList<>();
+    private String[][] tickets;
+    
+    public String[] solution(String[][] tickets) {
+        ticketCount = tickets.length;
+        isVisited = new boolean[ticketCount];
+        this.tickets = tickets;
+        
+        findRoutes(0, "ICN", "ICN");
+        
+        Collections.sort(routes);
+
+        return routes.get(0).split(" ");
+    }
+    
+    private void findRoutes(int count, String start, String path) {
+        if (ticketCount == count && isAllVisited()) {
+            routes.add(path);
+            return;
+        }
+        
+        for (int index = 0; index < ticketCount; index++) {
+            if (isVisited[index] || !tickets[index][0].equals(start)) {
+                continue;
+            }
+            
+            isVisited[index] = true;
+            findRoutes(count + 1, tickets[index][1], path + " " + tickets[index][1]);
+            isVisited[index] = false;
+        }
+    }
+    
+    private boolean isAllVisited() {
+        for (boolean visited : isVisited) {
+            if (!visited) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ programmers/[43164] 여행경로/java/README.md
@@ -0,0 +1,8 @@
+# [43164] 여행경로
+
+[문제 출처](https://school.programmers.co.kr/learn/courses/30/lessons/43164)
+
+## 성능
+
+- Time: 117.83ms
+- Memory: 114MB
